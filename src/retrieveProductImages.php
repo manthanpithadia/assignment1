ab8d9bd4ec380c7c0417c385b4fc2f8f185965a9
unknown <1787dhruv@gmail.com>
2022-02-27 22:17:02 -0500

    retrieveProductImages.php updated

---
+++ src/retrieveProductImages.php
@@ -0,0 +1,21 @@
+<?php
+   header('Access-Control-Allow-Origin: *');
+    include "connection.php";
+    $sql="select * from productimages";
+    $q=mysqli_query($con,$sql);
+
+    $rows=array();
+
+    if($q)
+    {        
+        while(($row=mysqli_fetch_assoc($q))!=null)
+        {
+        $rows['data'][]=$row;
+        }
+        print(json_encode($rows));
+    }
+    else
+    {
+    echo "try again";
+    }
+?>
